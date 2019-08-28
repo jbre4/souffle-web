590b21da2449fef86aa0256b59a0337ca6fa6f89
dlee0870@uni.sydney.edu.au <dlee0870@uni.sydney.edu.au>
2019-08-28 12:58:20 +1000

    Added index, and js for the website

---
+++ client/index.js
@@ -0,0 +1,31 @@
+function showData() {
+    var x = document.getElementById("input").value;
+    document.getElementById("output").innerHTML = x;
+}
+
+function sendData() {
+    var code = document.getElementById("input").value;
+    //document.getElementById("output").innerHTML = code;
+    $.ajax({url:"http://localhost",
+        type: "POST",
+        data: {code: code},
+        success: function() {
+            var succ = "Success";
+            document.getElementById("output").innerHTML = succ;
+        }, 
+        error: function() {
+            var err = "Error";
+            document.getElementById("output").innerHTML = err;
+        }
+    });
+
+    var data = new XMLHttpRequest();
+    data.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // Typical action to be performed when the document is ready:
+            document.getElementById("output").innerHTML = data.responseText;
+        }
+    };
+    data.open("GET", "output.txt", true);
+    data.send();
+}
