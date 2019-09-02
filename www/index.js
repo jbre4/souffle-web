ba5b88a4ee5691728ac2820d499abbc9714f8a49
dlee0870 <dlee0870@uni.sydney.edu.au>
2019-09-02 15:22:05 +1000

    Added lines to the input

--- www/index.js
+++ www/index.js
@@ -18,3 +18,10 @@ function do_post() {
 	xhr.setRequestHeader("Content-Type", post_mime);
 	xhr.send(post_body.value);
 }
+
+$(document).ready(function(){
+    var code = $(".codemirror-textarea")[0];
+    var editor = CodeMirror.fromTextArea(code, {
+        lineNumbers : true
+    });
+});
