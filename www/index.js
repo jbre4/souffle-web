de5e0d1a491ed7bff40a86c4eef5741247472205
Tiancheng Xia <tianchengxia@TianchengdeMBP.modem>
2019-09-04 03:00:08 +1000

    made a navigation bar for pages

--- www/index.js
+++ www/index.js
@@ -22,7 +22,7 @@ function do_post() {
 $(document).ready(function(){
     var code = $(".codemirror-textarea")[0];
     var editor = CodeMirror.fromTextArea(code, {
-		lineNumbers : true, 
+		lineNumbers : true,
 		mode: "python"
     });
 });
