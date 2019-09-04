37ec50b3d377e27dcfdc979832673e73e68eb66b
Tiancheng Xia <tianchengxia@TianchengdeMBP.modem>
2019-09-05 02:50:30 +1000

    modified the line counter and create a short cut for run button, resolve #14

--- www/cm/js/default.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    var code = $(".codemirror-textarea")[0];
-    var editor = CodeMirror.fromTextArea(code, {
-        lineNumbers : true
-    });
-});
--- www/index.js
+++ www/index.js
@@ -19,10 +19,50 @@ function do_post() {
 	xhr.send(post_body.value);
 }
 
-$(document).ready(function(){
-    var code = $(".codemirror-textarea")[0];
-    var editor = CodeMirror.fromTextArea(code, {
-		lineNumbers : true,
-		mode: "python"
-    });
+$("textarea").keypress(function(event) {
+        if (event.keyCode == 13 && event.shiftKey) {
+         do_post(); //Submit your form here
+         return false;
+         }
 });
+
+function syntaxHighlight() {
+  var text = document.getElementById("input").value;
+  console.log(text);
+  //still testing
+}
+
+var num="";
+keyUp();
+function keyUp(){
+  var input = document.getElementById("input");
+  var code = input.value;
+  code = code.replace(/\r/gi,"");
+  code = code.split("\n");
+  var n = code.length;
+  line(n);
+}
+function line(n){
+  var lineobj = document.getElementById("row_numbers");
+  for(var i=1;i<=n;i++){
+    if(document.all){
+      num+=i+"\r\n";
+    }else{
+      num+=i+"\n";
+    }
+  }
+  lineobj.value=num;
+  num="";
+}
+
+function autoScroll(){
+  var nV = 0;
+  if(!document.all) {
+    nV=document.getElementById("input").scrollTop;
+    document.getElementById("row_numbers").scrollTop=nV;
+    setTimeout("autoScroll()", 20);
+  }
+}
+if(!document.all){
+  window.addEventListener("load", autoScroll,false);
+}
